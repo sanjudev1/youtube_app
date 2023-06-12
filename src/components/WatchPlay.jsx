2d426eba3cec46f3c02883cb68cb1ae09aab1af6
sanjudev1 <sanjeevch.131@gmail.com>
2023-06-12 23:07:21 +0530

    main

--- src/components/WatchPlay.jsx
+++ src/components/WatchPlay.jsx
@@ -33,7 +33,7 @@ function WatchPlay() {
   
   return (
     <div className='w-full'><div className='flex'><div className={(menuOption===true) ?"ml-44":"m-5"}>
-      <iframe width="1160" 
+      <iframe width="1024" 
       height="615" 
       src={"https://www.youtube.com/embed/"+path_id}
        title="YouTube video player"
@@ -41,7 +41,7 @@ function WatchPlay() {
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
          allowFullScreen></iframe>
     </div>
-    <div className='w-full'><div className='text-black bg-white w-[29rem] h-[39rem] rounded-sm m-5 overflow-y-auto  flex flex-col-reverse'>
+    <div className='w-full'><div className='text-black bg-white w-[27rem] h-[39rem] rounded-sm m-5 overflow-y-auto  flex flex-col-reverse'>
      {chatMessages.map((each,index)=><div key={index} className='flex ml-3'><img className="h-10" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnPL1zW3xWnLePHIci-FRr8YsFF0Kh99rq3q9F4F_GJHu-dCxb7E2ZKsEEOF7LcsPgynU&usqp=CAU" alt="profile-icon"/><div className='font-bold ml-3 mt-1'><h1>{each.name}</h1><h1 className='font-semibold'>{each.text}</h1></div></div>)}
      </div><div>
       <form onSubmit={(e)=>{e.preventDefault(),dispatch(Addmessages({"name":"sanju","text":postmsg})),console.log(postmsg),setpostmg('')}}><input type="text" className='p-1 w-[20rem] ml-5' placeholder='chat with live...' value={postmsg} onChange={(e)=>setpostmg(e.target.value)}/> <button className='bg-white w-[98px] h-[34px] rounded-md text-lime-900'>Send</button></form></div><div/></div></div>
