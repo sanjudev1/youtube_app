ed732b87111a0e5555e768f235ab4e7a4a94dd0f
sanjeev ch <112742845+sanjudev1@users.noreply.github.com>
2024-01-11 13:17:58 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
   }, [searchQuery]);
 
   const getSuggestionsApi = async () => {
-    const API_URL = 'https://thingproxy.freeboard.io/fetch/https://suggestqueries.google.com/complete/search?client=firefox&q';
+    const API_URL = 'https://suggestqueries.google.com/complete/search?client=firefox&q';
     try {
       const data = await fetch(`${API_URL}=${searchQuery}`);
       const jsondata = await data.json();
